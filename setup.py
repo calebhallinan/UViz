28c1a59574c0ebdb75a3552472b492baae41c11f
Caleb Hallinan <cph4mb@virginia.edu>
2023-02-09 14:36:36 -0500

    add setup.py to make pip install later on, may need to add a couple of details still to this file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='UViz',
+    version='0.1.0',    
+    description='A Python Package with Post UMAP Visualization Functions',
+    url='https://github.com/calebhallinan/UViz',
+    author='Caleb Hallinan',
+    author_email='cph4mb@virginia.edu',
+    license='BSD 2-clause', # TODO: Change?
+    packages=['UViz'],
+    install_requires=['numpy']
+    # TODO: add classifiers?
+)
+
+
